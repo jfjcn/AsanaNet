bf68f7d369865d86236cc3f95bd9d41994a9255b
Fernando Cardenas <fernando@roicode.com>
2016-09-02 16:32:57 -0600

    Moved AsanaPersonalAccessToken into config

---
+++ RoiCode.AsanaDotNet/AsanaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+
+namespace RoiCode.AsanaDotNet
+{
+    public class AsanaRepository
+    {
+        private static readonly string AsanaBaseUrl = @"https://app.asana.com/api/1.0/";
+        private static readonly string AsanaPersonalAccessToken = ConfigurationManager.AppSettings["AsanaPersonalAccessToken"];
+
+        public AsanaUser GetMe()
+        {
+            var client = new RoiRestClient(AsanaBaseUrl, new RoiAsanaAuthenticator(AsanaPersonalAccessToken));
+            var result = client.GetSingle<AsanaUser>("users/me");
+            return result.ReturnedObject;
+        }
+
+
+    }
+}
