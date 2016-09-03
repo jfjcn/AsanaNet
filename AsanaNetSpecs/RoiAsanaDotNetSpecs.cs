e8cf3c558bb82a60f4ce22cc63888becebefd0c2
Fernando Cardenas <fernando@roicode.com>
2016-09-03 14:18:49 -0600

    Refactored the specs to get rid of some ancillary code

--- AsanaNetSpecs/RoiAsanaDotNetSpecs.cs
+++ AsanaNetSpecs/RoiAsanaDotNetSpecs.cs
@@ -7,41 +7,33 @@ namespace AsanaNetSpecs
     class When_using_the_Asana_Repository
     {
 
+        protected static AsanaRepository AsanaRepository = new AsanaRepository();
+
         [Test]
         public void _010_we_should_be_able_to_get_our_users_name()
         {
-            var asanaRepository = new AsanaRepository();
-            var myUser = asanaRepository.GetMe();
+            var myUser = AsanaRepository.GetMe();
             Assert.That(myUser.Name, Is.Not.Null.Or.Empty);
         }
 
-//        [Test]
-//        public void _011_we_should_be_able_to_get_our_users_name_again_without_throwing_an_exception()
-//        {
-//            Action<string, string, string> errorAction = GetFailedAction();
-//            var asanaService = GetAsanaService(errorAction);
-//            asanaService.GetMe(request =>
-//            {
-//                var user = request as AsanaUser;
-//                Assert.That(user.Name, Is.Not.Null.Or.Empty);
-//            }).Wait();
-//        }
-//
-//        [Test]
-//        public void _020_we_should_be_able_to_get_at_least_one_workspace()
-//        {
-//            Action<string, string, string> errorAction = GetFailedAction();
-//            var asanaService = GetAsanaService(errorAction);
-//            var hasWorkspaces = false;
-//            asanaService.GetWorkspaces(request =>
-//            {
-//                foreach (AsanaWorkspace asanaWorkspace in request)
-//                {
-//                    hasWorkspaces = true;
-//                }
-//            }).Wait();
-//            Assert.That(hasWorkspaces);
-//        }
+        [Test]
+        public void _011_we_should_be_able_to_get_our_users_name_again_without_throwing_an_exception()
+        {
+            var myUser = AsanaRepository.GetMe();
+            Assert.That(myUser.Name, Is.Not.Null.Or.Empty);
+        }
+
+        [Test]
+        public void _020_we_should_be_able_to_get_at_least_one_workspace()
+        {
+            var myWorkspaces = AsanaRepository.GetWorkspaces();
+            var hasWorkspaces = false;
+            foreach (AsanaWorkspace asanaWorkspace in myWorkspaces)
+            {
+                hasWorkspaces = true;
+            }
+            Assert.That(hasWorkspaces);
+        }
 
 
 
